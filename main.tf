99be808c55d8333cc0ebdef0b7ad5294c482e480
motez-baccouch <80569257+motez-baccouch@users.noreply.github.com>
2023-12-05 20:07:14 +0100

    terraform basic code

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "TrinoDaninoInstance" {
+  ami                    = "ami-0fc5d935ebf8bc3bc"
+  instance_type          = "t2.micro"
+  key_name      = "shunter_ubuntu_key"  # Le nom de la clé telle qu'enregistrée dans AWS EC2
+
+  tags = {
+    Name = "MonInstanceEC2FromJenkins"
+  }
+}
